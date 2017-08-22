ecbdd37d34c40a46d7af56bc726cd5902305c476
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-22 09:49:50 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DictionaryPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter name:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Please enter phone number associated with {name}:");
+            var phoneNum = Console.ReadLine();
+        }
+    }
+}
